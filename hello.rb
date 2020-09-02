0e51fd05d96a3f67995b1c5dbf839b22cdaad959
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-02 21:24:54 +0900

    first commit

---
+++ hello.rb
@@ -0,0 +1,39 @@
+# コメント
+
+=begin
+コメント
+コメント
+=end
+
+print "hello world"
+puts "hello world"  #改行あり
+p "hello world" #デバッグ用
+
+
+#変数
+# msg = "hello world"
+# puts msg
+
+# msg = "hello world again"
+# puts msg
+
+#定数は大文字
+# VERSION = 1.1
+# puts VERSION
+
+# VERSION = 1.2
+# puts VERSION
+
+p 4.8.class
+p 4.8.methods
+
+p 10 + 3
+p 10 * 3
+p 10 / 3
+p 10 % 3
+p 10.0 / 3
+p Rational(2, 5) * Rational(3, 5)
+p 2/5r + 3/5r
+p 53.6.round
+p 53.6.floor
+p 53.6.ceil
