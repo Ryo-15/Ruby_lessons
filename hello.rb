39d36c4c8be7436c1eed7cdc7f72f4e3e90b6797
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-04 16:29:51 +0900

    [finish]lesson19

--- hello.rb
+++ hello.rb
@@ -212,3 +212,18 @@
 # sayHi
 
 # p sayHi("tagucho")
+
+class User
+  def initialize(name)
+    @name = name
+  end
+  
+  def sayHi
+    puts "hi! i am #{@name}"
+  end
+end
+
+tom = User.new("tom")
+tom.sayHi
+bob = User.new("bob")
+bob.sayHi
