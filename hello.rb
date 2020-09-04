2d0414d3c92e4f4ad2962e648edae26632a8ec56
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-04 14:14:03 +0900

    [finish]lesson17

--- hello.rb
+++ hello.rb
@@ -158,24 +158,46 @@
 # end
 # 10.times { |i| puts "#{i}: hello" }
 
-# for
-for i in 15..20 do
-  p i
-end
-for color in ["red", "blue"] do
-  p color
-end
-for name, score in {taguchi:200, fkoji:400} do
-  puts "#{name}: #{score}"
-end
+# # for
+# for i in 15..20 do
+#   p i
+# end
+# for color in ["red", "blue"] do
+#   p color
+# end
+# for name, score in {taguchi:200, fkoji:400} do
+#   puts "#{name}: #{score}"
+# end
 
-# each
-(15..20).each do |i|
+# # each
+# (15..20).each do |i|
+#   p i
+# end
+# ["red", "blue"].each do |color|
+#   p color
+# end
+# {taguchi:200, fkoji:400}.each do |name, score|
+#   puts "#{name}: #{score}"
+# end
+
+# # loop
+# i = 0
+# loop do
+#   p i
+#   i += 1
+# end
+
+# break
+# next
+10.times do |i|
+  if i == 7
+    break
+  end
   p i
 end
-["red", "blue"].each do |color|
-  p color
-end
-{taguchi:200, fkoji:400}.each do |name, score|
-  puts "#{name}: #{score}"
+10.times do |i|
+  if i == 7
+    next
+  end
+  p i
 end
