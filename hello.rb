bb2dce1834f611d62ea98de4db0194a2ba5a1c78
Ryo-15 <tomorrows.way15@gmail.com>
2020-09-05 15:16:04 +0900

    [finish]lesson26

--- hello.rb
+++ hello.rb
@@ -309,21 +309,42 @@
 
 
 
-# module
-# ミックスイン
-module Debug
-  def info
-    puts "#{self.class} debug info ..."
-  end
+# # module
+# # ミックスイン
+# module Debug
+#   def info
+#     puts "#{self.class} debug info ..."
+#   end
   
-end
-class Player
-  include Debug
-end
+# end
+# class Player
+#   include Debug
+# end
 
-class Monster
-  include Debug
-end
+# class Monster
+#   include Debug
+# end
 
-Player.new.info
-Monster.new.info
+# Player.new.info
+# Monster.new.info
+
+
+# 例外
+class MyError < StandardError; end
+
+x = gets.to_i
+
+begin
+  if x == 3
+    raise MyError
+  end
+  p 100 / x
+rescue MyError
+  puts "not 3!"
+rescue => ex
+  p ex.message
+  p ex.class
+  puts "stopped!"
+ensure
+  puts "--end--"
+end
